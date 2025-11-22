027d36bc5c4a3663889712ab4e770a9d775eb323
listopadbogdan <listopad.bogdan@bk.ru>
2025-11-22 22:17:27 +0300

    Update Program.cs

--- Execute/Program.cs
+++ Execute/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+
 namespace Execute;
 
 public static partial class  Program
 {
     public static void Main()
     {
-        TestPriorityQeueue();
+        // TestPriorityQeueue();
+        TestHuffman();
+    }
+
+    public static void TestHuffman()
+    {
+
+        List<byte[]> list = new List<byte[]>();
+        while (true)
+        {
+            byte[] b = new byte[1_000_000];
+            Thread.Sleep(100);
+            list.Add(b);
+        }
+        string s = "beep boop beer!";
+        string huffman = HuffmanAlgo.Encode(s);
+        
+        Console.WriteLine(huffman);
     }
 }
